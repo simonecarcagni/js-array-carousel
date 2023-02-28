b6347c8d944723101a4755958d8e773bb9dbb439
Simone Carcagnì <simonecarcagni@libero.it>
2023-02-28 17:32:17 +0100

    Carosello Completato

--- js/main.js
+++ js/main.js
@@ -8,12 +8,61 @@ let imageArray = [
 
 const imagesDom = document.querySelector('.images');
 
+let imageContent = "";
+
 for (let i = 0; i < imageArray.length; i++){
 
-    let autoImages = `<div class="img">
+    let autoImages = `<div class="img-container">
                           <img src="${imageArray[i]}" >
                       </div>`;
 
-    imagesDom.innerHTML += autoImages;
+    imageContent += autoImages;
 }
 
+imagesDom.innerHTML = imageContent;
+
+console.log(imageContent);
+
+const buttonNextDom = document.querySelector('.btn-next');
+const buttonBackDom = document.querySelector('.btn-back');
+
+const imgContainerDom = document.getElementsByClassName('img-container');
+
+let showedImage = 0;
+
+imgContainerDom[showedImage].classList.add('showed');
+
+console.log(imgContainerDom);
+
+buttonNextDom.addEventListener('click', function(){
+    if (showedImage < imageArray.length - 1){
+
+        imgContainerDom[showedImage].classList.remove('showed');
+        showedImage++
+        imgContainerDom[showedImage].classList.add('showed')
+
+        buttonBackDom.classList.remove('hidden');
+
+        if (showedImage == imageArray.length - 1){
+            buttonNextDom.classList.add('hidden');
+        }
+    }
+  }
+);
+
+
+buttonBackDom.addEventListener('click', function () {
+    if (showedImage > 0) {
+
+        imgContainerDom[showedImage].classList.remove('showed');
+        showedImage--
+        imgContainerDom[showedImage].classList.add('showed')
+
+        buttonNextDom.classList.remove('hidden');
+
+        if (showedImage == 0) {
+            buttonBackDom.classList.add('hidden');
+        }
+    }
+}
+);
